acfd6f1d7813d45d1a22ac1cc98749d42a171d03
Ivan <latruga2@gmail.com>
2022-07-09 12:25:58 +0300

    Проект Tойота

--- src/project/Runner.java
+++ src/project/Runner.java
@@ -6,7 +6,10 @@ public class Runner {
     public static void main(String[] args) {
 
         Camry camry = new Camry("silver",250, 100000, true, Transmission.AUTOMATIC,
-               array(SizesOfWheel.SIXTEEN), new GasTank(50), new Engine(true), new Electric(true),
+               new Wheel[]{new Wheel(SizesOfWheel.SEVENTEEN, true),
+                           new Wheel(SizesOfWheel.SEVENTEEN, true),
+                           new Wheel(SizesOfWheel.SEVENTEEN, true),
+                           new Wheel(SizesOfWheel.SEVENTEEN, true)}, new GasTank(50), new Engine(true), new Electric(true),
                 new HeadLights(true), new UsbPort(true));
                 camry.startCar();
                 camry.musicOn();
@@ -16,7 +19,10 @@ public class Runner {
                 System.out.println();
 
         Hiance hiance = new Hiance("white", 150, 75000, true, Transmission.MECHANIC,
-                array(SizesOfWheel.SEVENTEEN), new GasTank(70), new Engine(true),
+                new Wheel[]{new Wheel(SizesOfWheel.SIXTEEN, true),
+                            new Wheel(SizesOfWheel.SIXTEEN, true),
+                            new Wheel(SizesOfWheel.SIXTEEN, true),
+                            new Wheel(SizesOfWheel.SIXTEEN, true)}, new GasTank(70), new Engine(true),
                 new Electric(true), new HeadLights(true), 3000,
                 new Wheel(SizesOfWheel.SEVENTEEN, true));
                 hiance.startCar();
@@ -25,7 +31,10 @@ public class Runner {
                 System.out.println();
 
         Solara solara = new Solara("black", 300, 200000, true, Transmission.ROBOT,
-                array(SizesOfWheel.TWENTY), new GasTank(60), new Engine(true),
+                new Wheel[]{new Wheel(SizesOfWheel.TWENTY, true),
+                            new Wheel(SizesOfWheel.TWENTY, true),
+                            new Wheel(SizesOfWheel.TWENTY, true),
+                            new Wheel(SizesOfWheel.TWENTY, true)}, new GasTank(60), new Engine(true),
                 new Electric(true), new HeadLights(true), true,
                 new MiniFridge());
                 solara.startCar();
@@ -36,20 +45,16 @@ public class Runner {
                 System.out.println();
 
         Dyna dyna = new Dyna("white", 150, 75000, true, Transmission.MECHANIC,
-                array(SizesOfWheel.SEVENTEEN), new GasTank(75), new Engine(true),
+                new Wheel[]{new Wheel(SizesOfWheel.SEVENTEEN, true),
+                            new Wheel(SizesOfWheel.SEVENTEEN, true),
+                            new Wheel(SizesOfWheel.SEVENTEEN, true),
+                            new Wheel(SizesOfWheel.SEVENTEEN, true)}, new GasTank(75), new Engine(true),
                 new Electric(true), new HeadLights(true), 4,
                 new Socket());
                 dyna.startCar();
                 dyna.HeadLightsOn();
                 dyna.chargePhone();
                 dyna.stopCar();
-    }
-    static Wheel[] array(SizesOfWheel sizesOfWheel) {
-        Wheel[] wheels = new Wheel[4];
-        for (int i = 0; i < wheels.length; i++) {
-            wheels[i] = new Wheel(sizesOfWheel, true);
-        }
-        return wheels;
     }
 }
 
